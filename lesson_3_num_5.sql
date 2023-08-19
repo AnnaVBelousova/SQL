1e045339af52170dc275a8f79336ed894bff661a
AnnaVBelousova <119129773+AnnaVBelousova@users.noreply.github.com>
2023-08-19 16:07:57 +0500

    Add files via upload

---
+++ lesson_3_num_5.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: select * from  mysatabase.orders where amt> 1
+LIMIT 0, 1000
+
+-- Date: 2023-08-19 15:57
+*/
+INSERT INTO `` (`onum`,`amt`,`odate`,`cnum`,`snum`) VALUES (3001,18.69,'1990-03-10',2008,1007);
+INSERT INTO `` (`onum`,`amt`,`odate`,`cnum`,`snum`) VALUES (3002,1900.1,'1990-03-10',2007,1004);
+INSERT INTO `` (`onum`,`amt`,`odate`,`cnum`,`snum`) VALUES (3003,767.19,'1990-03-10',2001,1001);
+INSERT INTO `` (`onum`,`amt`,`odate`,`cnum`,`snum`) VALUES (3005,5160,'1990-03-10',2008,1007);
+INSERT INTO `` (`onum`,`amt`,`odate`,`cnum`,`snum`) VALUES (3007,75.75,'1990-04-10',2004,1002);
+INSERT INTO `` (`onum`,`amt`,`odate`,`cnum`,`snum`) VALUES (3008,4723,'1990-05-10',2006,1001);
+INSERT INTO `` (`onum`,`amt`,`odate`,`cnum`,`snum`) VALUES (3009,1713.23,'1990-04-10',2002,1003);
+INSERT INTO `` (`onum`,`amt`,`odate`,`cnum`,`snum`) VALUES (3010,1309.95,'1990-06-10',2004,1002);
+INSERT INTO `` (`onum`,`amt`,`odate`,`cnum`,`snum`) VALUES (3011,9892,'1990-06-10',2006,1001);
